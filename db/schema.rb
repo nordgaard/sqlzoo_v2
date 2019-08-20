6927a546e7f36e1481a234dc59f4fac9cef747d2
Jens <jenordgaard@gmail.com>
2019-08-20 13:26:03 -0700

    Update schema.rb
    Added problem number attribute to Problems

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_08_20_200817) do
+ActiveRecord::Schema.define(version: 2019_08_20_202421) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -21,6 +21,7 @@ ActiveRecord::Schema.define(version: 2019_08_20_200817) do
     t.string "answer"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.integer "problem_number"
   end
 
 end
